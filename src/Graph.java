9b2fb87478d527ab9cc74c6d2c62cc0605914afc
Nisse <viktorbj@kth.se>
2015-03-24 12:05:15 +0100

    Added addEdge method in graph class, not finished however.

--- src/Graph.java
+++ src/Graph.java
@@ -17,5 +17,20 @@ public class Graph {
 		graph = new ArrayList<ArrayList<Integer>>(numEdges);
 	}
 	
+	public boolean addEdge(int from, int to, int weight)  {
+		// Sanitize the input.
+		if (from < 0 || from > (numNodes - 1)) {
+			System.err.println("Bad value of from: " + from + " in addEdge.");
+			return false;
+		} else if (to < 0 || to > (numNodes - 1)) {
+			System.err.println("Bad value of to: " + to + " in addEdge.");
+			return false;
+		} else if (weight < 0) {
+			System.err.println("Bad value of weight: " + weight + " in addEdge.");
+			return false;
+		}
+		//graph.get(to).get(from) = 
+		return true;
+	}
 	
 }
