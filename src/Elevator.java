ca5fc706fde7129e16298f8ce3ca7a03df1dd5d8
Nisse <viktorbj@kth.se>
2015-03-24 11:16:39 +0100

    Added description for the 'moving' field in the Elevator class.

--- src/Elevator.java
+++ src/Elevator.java
@@ -11,10 +11,12 @@ public class Elevator {
 	int prevNode;	// The previous node that the elevator visited.
 	int position;	// The progress of the elevator between two nodes.
 	int goal;		// The goal of the elevator, where it is travelling to.
-	int moving;
+	int moving;		// In which direction the elevator is moving. We will have four possible directions.
 	
 	public Elevator() {
 		
 	}
+	
+	
 	
 }
