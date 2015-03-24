8cdeb6969b9f53af739998fe26398f005b6e2764
Nisse <viktorbj@kth.se>
2015-03-24 11:27:05 +0100

    Added getters for the position fields in the Elevator class.

--- src/Elevator.java
+++ src/Elevator.java
@@ -9,7 +9,7 @@ public class Elevator {
 	// Private fields
 	int nextNode;	// The next node that the elevator will reach.
 	int prevNode;	// The previous node that the elevator visited.
-	int position;	// The progress of the elevator between two nodes.
+	double position;	// The progress of the elevator between two nodes.
 	int goal;		// The goal of the elevator, where it is travelling to.
 	int moving;		// In which direction the elevator is moving. We will have four possible directions.
 	
@@ -17,6 +17,39 @@ public class Elevator {
 		
 	}
 	
+	/*
+	 * Returns the next Node for this cabin.
+	 * A positive integer indicates that it is moving towards a new node,
+	 * -1 is when it is standing still.
+	 * @param: none
+	 * @return: int, the next node.
+	 */
+	public int getNextNode() {
+		return nextNode;
+	}
 	
+	/*
+	 * Returns the previous node for this cabin.
+	 * A positive integer indicates that it is moving away from this node,
+	 * -1 is when it is standing still.
+	 * @param: none
+	 * @return: int, the previous node.
+	 */
+	public int getPrevNode() {
+		return prevNode;
+	}
+	
+	/*
+	 * Returns the position of the elevator.
+	 * The position is a double that indicates
+	 * the progress between two nodes, the previous node and the next node.
+	 * The position will take values between zero and whatever the weight 
+	 * for the edge is between the nodes.
+	 * @param: none
+	 * @returns: double, the position.
+	 */
+	public double getPosition() {
+		return position;
+	}
 	
 }
