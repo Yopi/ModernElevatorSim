1ace42cb7e435149952a9ec8855e2e64667210b7
Nisse <viktorbj@kth.se>
2015-03-24 10:52:50 +0100

    Created java eclipse project and created a class for the Simulation, Elevator cabin and Graph.

---
+++ src/Elevator.java
@@ -0,0 +1,17 @@
+/*
+ * Class to represent the elevator cabins in the simulation.
+ * 
+ * Authors: Viktor Björkholm & Jesper Bränn
+ * Date: 2015-03-24
+ */
+public class Elevator {
+	
+	// Private fields
+	int position;	// This one will perhaps have to change.
+	int moving;
+	
+	public Elevator() {
+		
+	}
+	
+}
---
+++ src/Graph.java
@@ -0,0 +1,12 @@
+/*
+ * Class to represent a single directed graph with weighted edges to simulate length.
+ * 
+ * Authors: Viktor Björkholm & Jesper Bränn
+ * Date: 2015-03-24
+ */
+public class Graph {
+
+	
+	
+	
+}
