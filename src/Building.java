f615f31b7ec6b1e1c21a70f9d1814d7b55a98617
Nisse <viktorbj@kth.se>
2015-03-26 14:57:16 +0100

    Added the building class and (jspr pls) its fields and (jpr pls) two (pls) private classes for it to keep track of the elevators and persons.

---
+++ src/Building.java
@@ -0,0 +1,47 @@
+/*
+ * The building in the elevator project.
+ * Is updated with all the elevators and persons positions.
+ * 
+ * Authors: Viktor Björkholm & Jesper Bränn
+ * Date: 2015-03-26
+ */
+public class Building {
+	
+	public Building() {
+		
+	}
+	
+	public void addElevator() {
+		
+	}
+	
+	public void addPerson() {
+		
+	}
+	
+	public double readPosition(int elevatorId) {
+		return 0.0;
+	}
+	
+	public void updateElevatorPosition(int elevatorId, int position) {
+		
+	}
+	
+	private class Elevator {
+		
+		double position;
+		int nextNode;
+		int prevNode;
+		
+		public Elevator() {
+			
+		}
+	}
+	
+	private class Person {
+		
+		public Person() {
+			
+		}
+	}
+}
