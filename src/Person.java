cdfd03b269ab747979b1898b222abf4a8f03dd24
Nisse <viktorbj@kth.se>
2015-03-26 14:03:34 +0100

    Added the person class.

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+/*
+ * Person class.
+ * All you need to represent a person.
+ * Truly.
+ * 
+ * Authors: Viktor Björkholm & Jesper Bränn
+ * Date: 2015-03-26
+ */
+
+
+public class Person {
+	
+	int beginWork;
+	int endWork;
+	int lunchTime;
+	int[] meetingTimes;
+	int currentFloor;
+	
+	
+	
+	public Person() {
+		
+	}
+	
+}
