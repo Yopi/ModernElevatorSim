97d8163aa0b76349a8aae3e39b85f7fc8442f3f5
Nisse <viktorbj@kth.se>
2015-03-26 14:32:41 +0100

    Added the constructor and (jesper pls) fields for the person class.

--- src/Person.java
+++ src/Person.java
@@ -10,15 +10,33 @@
 
 public class Person {
 	
-	int beginWork;
-	int endWork;
-	int lunchTime;
-	int[] meetingTimes;
-	int currentFloor;
+	int beginWork;	// The time that the person begins work.
+	int endWork;	// The time when the person ends work. (leaves building)
+	int lunchTime;	// Time for the person to eat lunch (movement in building)
+	int[] meetingTimes;	// Meetings (movement in building)
+	int currentFloor;	// The position of the person.
+	int idleWait;
+	int travelWait;
+	int travelDist;
+	int currentTarget;
 	
 	
 	
-	public Person() {
+	public Person(int beginWork, int endWork, int lunchTime, int[] meetingTimes) {
+		this.beginWork = beginWork;
+		this.endWork = endWork;
+		this.lunchTime = lunchTime;
+		this.meetingTimes = new int[meetingTimes.length];
+		for (int i = 0; i < meetingTimes.length; i++) {
+			this.meetingTimes[i] = meetingTimes[i];
+		}
+		int idleWait = 0;
+		int travelWait = 0;
+		int travelDist = 0;
+		int currentTarget = -1;
+	}
+	
+	public void tick() {
 		
 	}
 	
