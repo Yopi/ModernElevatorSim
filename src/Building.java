94ef6af1adc512b999479b352db6be3852aa1f43
Nisse <viktorbj@kth.se>
2015-03-30 13:31:20 +0200

    Added fields for the elevators and persons in the building class.

--- src/Building.java
+++ src/Building.java
@@ -5,20 +5,27 @@
  * Authors: Viktor Björkholm & Jesper Bränn
  * Date: 2015-03-26
  */
+import java.util.ArrayList;
+
+
 public class Building {
 	
 	Graph graph;
+	ArrayList<Elevator> elevators;
+	ArrayList<Person> persons;
 	
 	public Building(Graph graph) {
 		this.graph = graph;
+		elevators = new ArrayList<Elevator>();
+		persons = new ArrayList<Person>();
 	}
 	
-	public void addElevator() {
-		
+	public void addElevator(Elevator e) {
+		elevators.add(e);
 	}
 	
-	public void addPerson() {
-		
+	public void addPerson(Person p) {
+		persons.add(p);
 	}
 	
 	public double readPosition(int elevatorId) {
