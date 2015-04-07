6cf6bbb1c91cab9d607f1a8b331c257ce9148d66
Jesper Bränn <jbrann@csc.kth.se>
2015-04-07 11:06:57 +0200

    Create statistics base, using SQLite

---
+++ src/Statistics.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import com.almworks.sqlite4java.*;
+
+/**
+ * 
+ * @author Viktor Björkholm & Jesper Bränn
+ * @version 2015-04-07 
+ *
+ */
+public class Statistics {
+	SQLiteConnection db;
+	double second;
+	
+	public Statistics(double second) {
+		this.second = second;
+		
+		File dbFile = new File("/tmp/database");
+		boolean newDB = (dbFile.exists()) ? false : true; 
+		db = new SQLiteConnection(dbFile);
+		try {	
+			db.open(true);
+			if(newDB) {
+				db.exec("BEGIN TRANSACTION");
+				db.exec("CREATE TABLE statistics (id INTEGER PRIMARY KEY AUTOINCREMENT, person_id INTEGER, type VARCHAR(255), duration REAL)");
+				db.exec("COMMIT");
+			}
+		} catch(SQLiteException e) {
+			System.err.println("Could not connect to the database");
+		}
+	}
+	
+	public void addWaitingTime(int personID, int ticksDuration) {
+		SQLiteStatement st = null;
+		try {
+			st = db.prepare("INSERT INTO statistics (person_id, type, duration) VALUES (?, ?, ?)");
+			st.bind(1, personID);
+			st.bind(2, "wait");
+			st.bind(3, ticksDuration * second);
+			st.step();
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
+	}
+	
+	public void addTravelTime(int personID, int ticksDuration) {
+		SQLiteStatement st = null;
+		try {
+			st = db.prepare("INSERT INTO statistics (person_id, type, duration) VALUES (?, ?, ?)");
+			st.bind(1, personID);
+			st.bind(2, "travel");
+			st.bind(3, ticksDuration * second);
+			st.step();
+		} catch (SQLiteException e) {
+			e.printStackTrace();
+		} finally {
+			st.dispose();
+		}
+	}
+}
