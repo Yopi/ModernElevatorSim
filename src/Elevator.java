ae8f8b813fbb2da3a512a9b15a2e37e4d9dd4d2e
Nisse <viktorbj@kth.se>
2015-04-15 15:48:55 +0200

    Removed job class from elevator class

--- src/Elevator.java
+++ src/Elevator.java
@@ -98,22 +98,6 @@ public class Elevator {
 		this.building = building;
 	}
 	
-	public class Job {
-		
-		public int from;	// From what node the job origins
-		public int to;		// To what node the job is finished.
-		public int id;		// The person that called for the job.
-		
-					// The job does not store path since this might change
-					// during the job if new jobs are added.
-				
-		public Job(int from, int to, int id) {
-			this.from = from;
-			this.to = to;
-			this.id = id;
-		}
-	}
-	
 	/*
 	 * The tick-method for the Elevator class.
 	 * Does what work that has to be done for each elevator.
