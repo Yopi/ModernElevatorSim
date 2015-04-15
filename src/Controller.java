806a4b0baeb5ba9c709a1945c4a15351f51ddb6b
Nisse <viktorbj@kth.se>
2015-04-15 10:39:24 +0200

    Added comments about work in controller. Let's do this!

--- src/Controller.java
+++ src/Controller.java
@@ -4,13 +4,27 @@
  * 
  * TODO:
  * 1. Figure out how the heuristics are used in the simulation (Code-wise)
- * 1a. How 2 hook into elevator / person
+ * check - 1a. How 2 hook into elevator / person
+ * 		This is fixed within the elevator.
  * 2. Implement a first heuristic
  * 3. Make sure to re-arange an elevators current jobs to prioritize among them.
  * 4. Figure out how to ensure fairness (a job is always taken care of)
  * 5. Tell an elevator to get a move on by adding a new job with only a target and
  * 		-1 as from, the elevator will be set to ignore these.
  * 6. Write tick method to check for elevators that should move.
+ * 
+ * Vad ska kontrollern göra.
+ * 1. Den ska para ihop request med hiss efter heurestiker.
+ * 		- När en hiss får ett nytt jobb måste kontrollern ordna
+ * 		så att den placerar jobben i ordningen som hissen ska färdas.
+ * 		Detta borde gå att lösa med en slags sökning över hur sträckan
+ * 		minimeras. Typ en omväg för att lämna av en person för att det
+ * 		ska gå lättare på annat håll. Eller, omvägar kan behövas
+ *		tas för att den totala sträckan ska bli lägre. Om den har
+ *		koppling till building (som den måste ha för att läsa om
+ *		en hiss ska flytta på sig, se punkt 2) så kan den
+ *		ta reda på alla avstånd som den behöver kunna ta reda på.
+ * 2. Den ska kontrollera om några hissar fått förfrågan för att flytta på sig
  * 
  * 
  * Authors: Viktor Björkholm & Jesper Bränn
